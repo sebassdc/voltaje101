d7d02c362d907ded0f2de080d3c18b8fd1d980be
Sebastian Hurtado <sebassdc@gmail.com>
2018-06-27 10:37:04 -0400

    WIP: Routing started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { BrowserRouter as Router  } from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        test
+        <Router>
+          
+        </Router>
       </div>
     );
   }
