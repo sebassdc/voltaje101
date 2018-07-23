70fd3da02f7fa35f567d90d05c253a5cf2000df8
Sebastian Hurtado <sebassdc@gmail.com>
2018-07-23 14:16:02 -0400

    Advances on auth

--- src/index.js
+++ src/index.js
@@ -4,38 +4,27 @@ import './index.css';
 import App from './App';
 import { applyMiddleware, compose, createStore } from 'redux'
 import { Provider } from 'react-redux'
-import { connectRouter, routerMiddleware, ConnectedRouter } from 'connected-react-router'
-import { createBrowserHistory } from 'history'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 import registerServiceWorker from './registerServiceWorker';
 import rootReducer from './redux/reducers'
 
-const history = createBrowserHistory()
-
-const reducer = compose(
-  connectRouter(history),
-)(rootReducer)
-
 const enhancer = compose(
   applyMiddleware(
-    routerMiddleware(history),
     logger,
     thunk,
   )
 )
 
 const store = createStore(
-  reducer,
+  rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   enhancer
 ) 
 
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
       <App />
-    </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
 );
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import { Switch, Route } from 'react-router'
-import Editor from '../containers/Editor' 
+import { BrowserRouter as Router , Route } from 'react-router-dom'
 
 import AuthPage from '../containers/AuthPage'
 import Dashboard from '../containers/Dashboard'
 import Contact from '../containers/Contact'
 
 const routes = () =>
-  <Switch>
-    <Route exact path="/" component={AuthPage} />
-    <Route exact path="/dashboard" component={Dashboard} />
-    <Route exact path="/contacto" component={Contact} />
-  </Switch>
+  <Router>
+    <React.Fragment>
+      <Route exact path="/" component={AuthPage} />
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/contacto" component={Contact} />
+    </React.Fragment>
+  </Router>
 
 export default routes
